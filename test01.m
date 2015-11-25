2f728bbb698e583be4ebd0f4b12a431fab65c2cf
Hiroyuki Sogo <hsogo12600@gmail.com>
2015-11-25 19:50:33 +0900

    MOD: update test script

--- test01.m
+++ test01.m
@@ -111,98 +111,109 @@ try
 			%Leave from loop if calibration has been performed (res{2}==1).
 			break; 
 		end
-	end
-	
-	%-----------------------------------------------------------------
-	% Recording.
-	%   If space key is pressed, a message 'Space' is inserted to the 
-	%   data file and latest 6 samples of gaze position is transferred
-	%   from SimpleGazeTracker.
-	%   Current gaze position is transferred every frame and a white 
-	%   square is drawn at the current gaze position.
-	%-----------------------------------------------------------------
-	gazeposlist = {};
-	geteyeposdelaylist = [];
-    flipdelaylist = [];
-	previousKeyPressTime = GetSecs();
-	targetColor = 255;
-	%Start recording.
-	SimpleGazeTracker('StartRecording','Test trial',0.1);
-
-    fst = GetSecs();
-	for q = 1:360 %360 frames
-		[keyIsDown, secs, keyCode, deltaSecs] = KbCheck();
-		if keyCode(KbName('Space'))==1
-			% prevent chattering...
-			if GetSecs()-previousKeyPressTime > 0.2
-				SimpleGazeTracker('SendMessage','Space');
-				%get the latest 6 samples.
-				tmp = SimpleGazeTracker('GetEyePositionList',6,0,0.02);
-				if ~isempty(tmp)
-					gazeposlist(length(gazeposlist)+1) = {tmp};
-				end
-				%update previousKeyPressTime
-				previousKeyPressTime = GetSecs();
-				%change target color
-				if targetColor==255
-					targetColor=0;
-				else
-					targetColor=255;
-				end
-			end
-		end
-		if mod(q,60)==0
-			%Send message every 60 frames.
-			SimpleGazeTracker('SendMessage',num2str(q));
-		end
-		
-		st = GetSecs();
-		%get current gaze position (moving average of 3 samples).
-		pos = SimpleGazeTracker('GetEyePosition',3,0.02);
-		geteyeposdelaylist = [geteyeposdelaylist, 1000*(GetSecs()-st)];
-		
-		stimx = 200*cos(q/180*pi)+cx;
-		stimy = 200*sin(q/180*pi)+cy;
-		%horizontal component of current gaze position
-		markerx = pos{1}(1);
-		%vertical component of current gaze position
-		markery = pos{1}(2);
-		Screen('FillRect',wptr,127);
-		Screen('FillRect',wptr,0,[stimx-5,stimy-5,stimx+5,stimy+5]);
-		%draw marker at the current gaze position.
-		Screen('FillRect',wptr,targetColor,[markerx-5,markery-5,markerx+5,markery+5]);
-		Screen('Flip',wptr);
-        flipdelaylist = [flipdelaylist, 1000*(GetSecs()-fst)];
-        fst = GetSecs();
-	end
-	%Stop recording.
-	SimpleGazeTracker('StopRecording','',0.1);
-	
-	%-----------------------------------------------------------------
-	% Clear Screen
-	%-----------------------------------------------------------------
-	Screen('FillRect',wptr,127);
-	Screen('Flip',wptr);
-	
-	%-----------------------------------------------------------------
-	% Transfer data from SimpleGazeTracker.
-	%-----------------------------------------------------------------
-	fid = fopen([logfilename, '.txt'],'wt');
-	
-	%Output delay of SimpleGazeTracker('GetEyePosition')
-	fprintf(fid,'Delay of SimpleGazeTracker(''GetEyePosition'')\n');
-	for i=1:length(geteyeposdelaylist)
-		fprintf(fid,'%f\n',geteyeposdelaylist(i));
     end
     
-	%Output delay of Screen('Flip')
-	fprintf(fid,'Delay of Screen(''Flip'')\n');
-	for i=1:length(flipdelaylist)
-		fprintf(fid,'%f\n',flipdelaylist(i));
-	end
     
+	fid = fopen([logfilename, '.txt'],'wt');    
+    for block = 0:4
+        nRectDraw = 2^(2*block);
+        
+        %-----------------------------------------------------------------
+        % Recording.
+        %   If space key is pressed, a message 'Space' is inserted to the
+        %   data file and latest 6 samples of gaze position is transferred
+        %   from SimpleGazeTracker.
+        %   Current gaze position is transferred every frame and a white
+        %   square is drawn at the current gaze position.
+        %-----------------------------------------------------------------
+        gazeposlist = {};
+        geteyeposdelaylist = [];
+        flipdelaylist = [];
+        previousKeyPressTime = GetSecs();
+        targetColor = 255;
+        %Start recording.
+        SimpleGazeTracker('StartRecording',['nRectDraw:', num2str(nRectDraw)],0.1);
+        
+        fst = GetSecs();
+        for q = 1:360 %360 frames
+            [keyIsDown, secs, keyCode, deltaSecs] = KbCheck();
+            if keyCode(KbName('Space'))==1
+                % prevent chattering...
+                if GetSecs()-previousKeyPressTime > 0.2
+                    SimpleGazeTracker('SendMessage','Space');
+                    %get the latest 6 samples.
+                    tmp = SimpleGazeTracker('GetEyePositionList',6,0,0.02);
+                    if ~isempty(tmp)
+                        gazeposlist(length(gazeposlist)+1) = {tmp};
+                    end
+                    %update previousKeyPressTime
+                    previousKeyPressTime = GetSecs();
+                    %change target color
+                    if targetColor==255
+                        targetColor=0;
+                    else
+                        targetColor=255;
+                    end
+                end
+            end
+            if mod(q,60)==0
+                %Send message every 60 frames.
+                SimpleGazeTracker('SendMessage',num2str(q));
+            end
+            
+            st = GetSecs();
+            %get current gaze position (moving average of 3 samples).
+            pos = SimpleGazeTracker('GetEyePosition',3,0.02);
+            geteyeposdelaylist = [geteyeposdelaylist, 1000*(GetSecs()-st)];
+            
+            stimx = 200*cos(q/180*pi)+cx;
+            stimy = 200*sin(q/180*pi)+cy;
+            %horizontal component of current gaze position
+            markerx = pos{1}(1);
+            %vertical component of current gaze position
+            markery = pos{1}(2);
+            for nn = 1:nRectDraw
+                Screen('FillRect',wptr,127);
+            end
+            Screen('FillRect',wptr,0,[stimx-5,stimy-5,stimx+5,stimy+5]);
+            %draw marker at the current gaze position.
+            Screen('FillRect',wptr,targetColor,[markerx-5,markery-5,markerx+5,markery+5]);
+            Screen('Flip',wptr);
+            flipdelaylist = [flipdelaylist, 1000*(GetSecs()-fst)];
+            fst = GetSecs();
+        end
+        %Stop recording.
+        SimpleGazeTracker('StopRecording','',0.1);
+        
+        %-----------------------------------------------------------------
+        % Clear Screen
+        %-----------------------------------------------------------------
+        Screen('FillRect',wptr,127);
+        Screen('Flip',wptr);
+        
+        %-----------------------------------------------------------------
+        % Output
+        %-----------------------------------------------------------------
+        
+        %Output delay of SimpleGazeTracker('GetEyePosition')
+        fprintf(fid,['nRectDraw:',num2str(nRectDraw),'\n']);
+        fprintf(fid,'Delay of SimpleGazeTracker(''GetEyePosition'')\n');
+        for i=1:length(geteyeposdelaylist)
+            fprintf(fid,'%f\n',geteyeposdelaylist(i));
+        end
+        
+        %Output delay of Screen('Flip')
+        fprintf(fid,'Delay of Screen(''Flip'')\n');
+        for i=1:length(flipdelaylist)
+            fprintf(fid,'%f\n',flipdelaylist(i));
+        end
+        
+        fprintf(fid,'\n');    
+    end
+
     fclose(fid);
-	
+    
+
 	%-----------------------------------------------------------------
 	% Close remote data file and network connection.
 	%-----------------------------------------------------------------
